9c5b9257ce6be3ae670a92fb8944195e0b325512
Shunya Kobayashi <shunya14dev@gmail.com>
2025-08-19 00:08:56 +0900

    Initial commit for IT用語アプリ

---
+++ TermApp.DbTest/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using MySql.Data.MySqlClient;
+using System.Globalization;
+using System.Threading;
+
+class Program
+{
+    static void Main()
+    {
+        //db接続のテスト用のプロジェクト
+        Thread.CurrentThread.CurrentUICulture = CultureInfo.InvariantCulture;
+
+        string logPath = @"C:\appl\NewsApp\DbTest_log.txt"; 
+        // ↑出力ログファイル パス 列挙されて登録されていきます。
+
+        try
+        {
+            string cs = "server=localhost;user=root;password=pass;database=news_app;";
+            using var con = new MySqlConnection(cs);
+            con.Open();
+
+            File.AppendAllText(logPath, "ok\n");
+        }
+        catch (Exception ex)
+        {
+            File.AppendAllText(logPath, $"❌ 接続エラー：{ex.Message}\n");
+        }
+    }
+}
---
+++ TermApp/Services/MySqlService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Data;
+using MySql.Data.MySqlClient;
+
+namespace TermApp.Services
+{
+    public class MySqlService
+    {
+        private readonly string _connectionString =
+            "server=localhost;user=root;password=pass;database=news_app;";
+
+        public DataTable GetSummaries()
+        {
+            // DataTable はノンジェネリックな表形式オブジェクト
+            var table = new DataTable();
+
+            using (var conn = new MySqlConnection(_connectionString))
+            {
+                conn.Open();
+                string sql =
+                  "SELECT news, created_at " +
+                  "FROM day_news " +
+                  "ORDER BY created_at DESC " +
+                  "LIMIT 10;";
+
+                using (var cmd = new MySqlCommand(sql, conn))
+                using (var reader = cmd.ExecuteReader())
+                {
+                    // DataTable に reader の結果を丸ごと読み込む
+                    table.Load(reader);
+                }
+            }
+
+            return table;
+        }
+    }
+}

6d4f21772216068568616d1a390cb6a78a8641f8
Shunya Kobayashi <shunya14dev@gmail.com>
2025-08-22 06:53:40 +0900

    DBコネクションのパラメーター化を実施

---
+++ TermApp/Data/DbParameters.cs
@@ -0,0 +1,11 @@
+﻿// TermApp/Data/DbParameters.cs
+namespace TermApp.Data;
+
+public static class DbParameters
+{
+    public const string Host = "localhost";
+    public const uint Port = 3306;
+    public const string Database = "term_app";
+    public const string UserId = "root";
+    public const string Password = "pass";
+}
